948d15d4bff954e845cbe9f9890aad05381b0835
Mike Bild <mike.bild@googlemail.com>
2017-11-18 09:08:48 +0100

    Add some integration tests

---
+++ test/server.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const fetch = require('node-fetch');
+
+const linklet = require('../lib');
+const sut = linklet((req, res) => ({message: 'Hello World from Linklet!'}));
+
+describe('Linklet as module', () => {
+  let instance = null;
+
+  before(() => (instance = sut.listen()));
+  after(() => instance.close());
+
+  it('Should respone with status code 200', () => {
+    return fetch(
+      `http://localhost:${instance.address().port}`
+    ).then(response => {
+      assert.equal(response.status, 200);
+    });
+  });
+
+  it('Should respone JSON with default object', () => {
+    return fetch(`http://localhost:${instance.address().port}`)
+      .then(response => response.json())
+      .then(data => {
+        assert.deepEqual(data, {message: 'Hello World from Linklet!'});
+      });
+  });
+});
